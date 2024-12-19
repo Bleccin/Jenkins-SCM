9e4849392e93d19d72e49b6bf7444286be8517db
Blessing Awotokun <69427384+Bleccin@users.noreply.github.com>
2024-12-19 16:16:51 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8081;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Docker!');
+});
+
+app.listen(port, '0.0.0.0', () => { // This line is essential!
+  console.log(`Server listening on port ${port}`);
+});
